abd0e6a1585bf459f4d644de3c17bc95b023776e
kenwu <kenwu@gpmcorp.com.tw>
2022-06-20 09:45:51 +0800

    新增bat檔

---
+++ CloneProjects.bat
@@ -0,0 +1,3 @@
+call git clone http://gitlab.gpmcorp.com.tw/IoT_Project/distribute_monitor_system/edgemodels.git
+
+pause
---
+++ PullRemoteMaster.bat
@@ -0,0 +1,10 @@
+call git fetch
+call git checkout master
+call git pull origin master 
+
+cd edgemodels
+call git fetch
+call git checkout master
+call git pull origin master 
+
+pause
